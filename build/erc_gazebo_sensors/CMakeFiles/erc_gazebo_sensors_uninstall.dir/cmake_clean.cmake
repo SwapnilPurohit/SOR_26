7355bddb3be8f6a062ace12ca0da208f9db9ea05
Sachin <sachinmandal3580@gmail.com>
2026-06-14 14:40:46 +0530

    done

--- build/erc_gazebo_sensors/CMakeFiles/erc_gazebo_sensors_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/erc_gazebo_sensors_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/erc_gazebo_sensors_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/trajectory_server/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/sachin/Downloads/sensors_and_perception/install/trajectory_server'
